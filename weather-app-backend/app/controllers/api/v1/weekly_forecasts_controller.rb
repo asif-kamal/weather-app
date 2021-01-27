88193ca6ac42a32bf44bb2cd2ce7bdbfa5db20fa
asif-kamal <asif.kamal1489@gmail.com>
2021-01-27 17:33:53 -0500

    Added index action for weekly_forecasts controller

--- weather-app-backend/app/controllers/api/v1/weekly_forecasts_controller.rb
+++ weather-app-backend/app/controllers/api/v1/weekly_forecasts_controller.rb
@@ -1,3 +1,7 @@
 class Api::V1::WeeklyForecastsController < ApplicationController
    
+    def index
+        weekly_forecasts = WeeklyForcasts.all
+        render json: @weekly_forecasts
+    end
 end
