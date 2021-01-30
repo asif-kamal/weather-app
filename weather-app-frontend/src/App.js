faf101047c0f3f96868e990fb44e4aa22fa5660a
asif-kamal <asif.kamal1489@gmail.com>
2021-01-30 17:35:08 -0500

    Bound handleSubmit.

--- weather-app-frontend/src/App.js
+++ weather-app-frontend/src/App.js
@@ -9,6 +9,8 @@ class App extends React.Component {
     dailyForecast: []
     }
 
+    handleSubmit = this.handleSubmit.bind(this)
+
     render (){
       return (
         <div>
@@ -18,23 +20,25 @@ class App extends React.Component {
       )
       }
     
+      
 
   handleSubmit(event) {
     event.preventDefault()
     const city = event.target.city.value;
     const APIKEY = process.env['REACT_APP_WEATHER_API_KEY']
-        const WEATHER_URL = `api.openweathermap.org/data/2.5/forecast?q=${city}&units=imperial&appid=${APIKEY}`
+        const WEATHER_URL = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=imperial&appid=${APIKEY}`
         fetch(WEATHER_URL)
             .then(res => res.json())
             .then(data => {const dailyData = data.list.filter(reading => reading.dt_txt.includes("06:00:00"))
-            console.log(dailyData)
               this.setState({
               weeklyForecast: data.list,
               dailyForecast: dailyData
-            })
+            }, () => console.log(this.state))
           })
         }
            
+
+    
       }
 
   
