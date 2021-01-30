d6b4806015326f3d7e2d7b5f4a3f9d56f065f97b
asif-kamal <asif.kamal1489@gmail.com>
2021-01-29 21:03:08 -0500

    Added handleSubmit function.

--- weather-app-frontend/src/App.js
+++ weather-app-frontend/src/App.js
@@ -1,11 +1,48 @@
 import React from 'react'
 import WeeklyForecastContainer from './containers/WeeklyForecastContainer'
+import SearchBar from './components/SearchBar'
 
-function App(props){
-  return (
-    <WeeklyForecastContainer />
-  )
-}
+class App extends React.Component {
+
+  state = {
+    weeklyForecast: [],
+    dailyForecast: []
+    }
+
+    render (){
+      return (
+        <div>
+        <WeeklyForecastContainer />
+        <SearchBar getData={this.handleSubmit}/>
+        </div>
+      )
+      }
+    
+
+  handleSubmit(event) {
+    event.preventDefault()
+    const city = event.target.city.value;
+    const APIKEY = process.env['REACT_APP_WEATHER_API_KEY']
+        const WEATHER_URL = `api.openweathermap.org/data/2.5/forecast?q=${city}&units=imperial&appid=${APIKEY}`
+        fetch(WEATHER_URL)
+            .then(res => res.json())
+            .then(data => {const dailyData = data.list.filter(reading => reading.dt_txt.includes("06:00:00"))
+            console.log(dailyData)
+              this.setState({
+              weeklyForecast: data.list,
+              dailyForecast: dailyData
+            })
+          })
+        }
+           
+      }
+
   
+      
+  
+
+
+  
+
 
 export default App;
