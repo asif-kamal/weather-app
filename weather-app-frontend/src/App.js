b0b74ab5dea4ab63b82a60024717d1acfd708dcf
asif-kamal <asif.kamal1489@gmail.com>
2021-02-05 17:26:52 -0500

    Added router

--- weather-app-frontend/src/App.js
+++ weather-app-frontend/src/App.js
@@ -1,38 +1,27 @@
 import React from 'react'
 import WeeklyForecastContainer from './containers/WeeklyForecastContainer'
-import SearchForm from './components/SearchForm'
 
-class App extends React.Component {
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
+function App() {
   
-    handleSubmit = this.handleSubmit.bind(this)
-
-    handleSubmit(event) {
-      event.preventDefault()
-      const city = event.target.city.value;
-      const APIKEY = process.env['REACT_APP_WEATHER_API_KEY']
-          const WEATHER_URL = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=imperial&appid=${APIKEY}`
-          fetch(WEATHER_URL)
-              .then(res => res.json())
-              .then(data => {const dailyData = data.list.filter(reading => reading.dt_txt.includes("06:00:00"))
-                this.setState({
-                weeklyForecast: data.list,
-                dailyForecast: dailyData
-              }, () => console.log(this.state))
-            })
-          }
-
-
-    render (){
-      return (
-        <div>
-        <WeeklyForecastContainer forecastData={this.state}/>
-        <SearchForm getData={this.handleSubmit}/>
-        </div>
-      )
-    }    
-
   
-  }
+ return(<>
+
+    <Router>
+      <Switch>
+        <Route exact path={"/"} component={WeeklyForecastContainer} />
+       
+      </Switch>
+    </Router>
+    </>
+  
+ );
+ }
+
 
 export default App;
+
+
+
+
